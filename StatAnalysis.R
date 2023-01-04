30dcb7a1b42b30a62fe0d7e5cc216e64caab961f
martie <andres-martinez@uiowa.edu>
2023-01-04 14:44:14 -0600

    Fix format

--- StatAnalysis.R
+++ StatAnalysis.R
@@ -1,3 +1,7 @@
+# Statistical analysis to review data, mostly between control
+# and experiments, same time points.
+
+
 # Packages and libraries --------------------------------------------------
 # Install packages
 install.packages("dplyr")
@@ -15,41 +19,32 @@ exp.data.0 <- read.csv("PCBDataV02.csv")
 
 # Organize data -----------------------------------------------------------
 # Remove lost sample(s), NA
-exp.data <- exp.data.0
-exp.data <- exp.data.0[!is.na(exp.data.0$PCB4), ]
+exp.data <- exp.data.0[!(rowSums(exp.data.0[, c(8:180)],
+                                   na.rm = TRUE)==0),]
 
 # spme = SPME fiber sampler
 # puf = PUF sampler
 
 # Pull congener-specific data from the dataset & calculate mean
 # values for each sampler-treatment combination at each time point
-exp.mspme.control <- exp.data %>%
-  filter(treatment == "Ctrl" & sampler == "SPME") %>%
-  group_by(time) %>%
+exp.mspme <- exp.data %>%
+  filter(sampler == "SPME") %>%
+  group_by(time, treatment) %>%
   mutate(PCB4/length) %>%
   distinct(`PCB4/length`) %>%
-  rename("PCB 4 mass in SPME (ng/cm); Control" = `PCB4/length`)
+  rename("PCB 4 mass in SPME (ng/cm)" = `PCB4/length`)
 
-exp.mspme.LB400 <- exp.data %>%
-  filter(treatment == "LB400" & sampler == "SPME") %>%
-  group_by(time) %>%
-  mutate(PCB4/length) %>%
-  distinct(`PCB4/length`) %>%
-  rename("PCB 4 mass in SPME (ng/cm); LB400" = `PCB4/length`) 
+exp.mpuf <- exp.data %>%
+  filter(sampler == "PUF") %>%
+  group_by(time, treatment) %>%
+  mutate(PCB4) %>%
+  distinct(`PCB4`) %>%
+  rename("PCB 4 mass in PUF (ng)" = `PCB4`)
 
-exp.mpuf.control <- exp.data %>%
-  filter(treatment == "Ctrl" & sampler == "PUF") %>%
-  group_by(time) %>%
-  mutate(PCB52) %>%
-  distinct(`PCB52`) %>%
-  rename("PCB 4 mass in PUF (ng); Control" = `PCB52`)
 
-exp.mpuf.LB400 <- exp.data %>%
-  filter(treatment == "LB400" & sampler == "PUF") %>%
-  group_by(time) %>%
-  mutate(PCB52) %>%
-  distinct(`PCB52`) %>%
-  rename("PCB 4 mass in PUF (ng); LB400" = `PCB52`)
+
+
+
 
 PCB.4 <- cbind(exp.mspme.control, exp.mspme.LB400$`PCB 4 mass in SPME (ng/cm); LB400`,
                exp.mpuf.control$`PCB 4 mass in PUF (ng); Control`, exp.mpuf.LB400$`PCB 4 mass in PUF (ng); LB400`)
@@ -58,7 +53,7 @@ pCB.4 <-   add_row(time = 0, "PCB 4 mass in SPME (ng/cm); Control" = 0,
                    "PCB 4 mass in SPME (ng/cm); LB400" = 0,
                    "PCB 4 mass in PUF (ng); LB400" = 0, .before = 1)
 
-a <- inner_join(exp.mspme.control, exp.mspme.LB400, by = "time")
+a <- inner_join(exp.mpuf.control, exp.mpuf.LB400, by = "time")
 
 
 a <- left_join(exp.mspme.control %>% group_by('time') %>% mutate(id = row_number()),
