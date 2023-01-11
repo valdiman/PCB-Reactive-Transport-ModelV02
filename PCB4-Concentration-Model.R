16b2d8d32ac554b7294107f232651aa7451d53ba
martie <andres-martinez@uiowa.edu>
2023-01-11 10:57:58 -0600

    Small format changes

--- PCB4-Concentration-Model.R
+++ PCB4-Concentration-Model.R
@@ -20,14 +20,14 @@ rtm.PCB4 = function(t, c, parms){
   
   # Bioreactor parameters
   Vw <- 100 # cm3 water volume
-  Va <- 125 # cm3 headspace volume
-  Aaw <- 20 # cm2 
-  Aws <- 30 # cm2
+  Va <- 125 # cm3 head-space volume
+  Aaw <- 20 # cm2 air-water area
+  Aws <- 30 # cm2 sediment-water area
   
   # Congener-specific constants
   Kaw <- 0.01344142 # PCB 4 dimensionless Henry's law constant @ 25 C
   dUaw <- 49662.48 # internal energy for the transfer of air-water for PCB 4 (J/mol)
-  logKow <- 4.65 # PCB 4 octanol-water equilibrium partition coefficient
+  Kow <- 10^(4.65) # PCB 4 octanol-water equilibrium partition coefficient
   dUow <- -21338.96 # internal energy for the transfer of octanol-water for PCB 4 (J/mol)
   
   # Air & water physical conditions
@@ -38,6 +38,7 @@ rtm.PCB4 = function(t, c, parms){
   v.H2O <- 0.010072884	# cm2/s kinematic viscosity of water @ Tair = 25
   V.water.air <- 0.003 # m/s water's velocity of air-side mass transfer without ventilation (eq. 20-15)
   V.co2.w <- 4.1*10^-2 # m/s mass transfer coefficient of CO2 in water side without ventilation
+  # V.co2.w.2 <- 9*10^(-4)/100 # m/s new book 19-20
   SC.pcb.w <- v.H2O/D.pcb.water # Schmidt number PCB 4
   bl <- 0.21 # cm boundary layer thickness
   
@@ -47,7 +48,8 @@ rtm.PCB4 = function(t, c, parms){
   # ii) Kaw.a, air-side mass transfer coefficient
   Kaw.a <- V.water.air*(D.pcb.air/D.water.air)^(0.67) # [m/s]
   # iii) Kaw.w, water-side mass transfer coefficient for PCB 4. 600 is the Schmidt number of CO2 at 298 K
-  Kaw.w <- V.co2.w*(SC.pcb.w/600)^(-0.5) # [m/s] 
+  Kaw.w <- V.co2.w*(SC.pcb.w/600)^(-0.5) # [m/s]
+  # Kaw.w.2 <- V.co2.w.2*(SC.pcb.w/600)^(-0.5) # [m/s]
   # iv) kaw, overall air-water mass transfer coefficient for PCB 4
   kaw.o <- (1/(Kaw.a*Kaw.t) + (1/Kaw.w))^-1 # [m/s]
   # v) kaw, overall air-water mass transfer coefficient for PCB 4, units change
@@ -56,16 +58,18 @@ rtm.PCB4 = function(t, c, parms){
   # Estimating Cpw (PCB 4 concentration in sediment porewater)
   Ct <- 630.2023 # ng/g PCB 4 sediment concentration
   foc <- 0.03 # organic carbon % in sediment
-  Kow <- 10^(logKow)
-  Kow.t <- Kow*exp(-dUow/R*(1/Tw.1-1/Tst.1))
-  logKoc <- 0.94*log10(Kow.t) + 0.42
+  Kow.t <- Kow*exp(-dUow/R*(1/Tw.1-1/Tst.1)) # temperature correction
+  logKoc <- 0.94*log10(Kow.t) + 0.42 # koc calculation
   Kd <- foc*10^(logKoc) # L/kg sediment-water equilibrium partition coefficient
   Cpw <- Ct/Kd*1000 # [ng/L]
+  
+  # Biotransformation rate
+  kb <- 0 #0.130728499 # 1/d, value changes depending on experiment, i.e., control = 0, treatments LB400 = 0.130728499
   
   # derivatives dx/dt are computed below
   r <- rep(0,length(c))
   # dCwdt:
-  r[1] <- kaw.o*Aaw/Vw*(c["Ca"]/(Kaw.t) - c["Cw"]) + D.pcb.water*Aws*60*60*24/bl/Vw*(Cpw - c["Cw"])
+  r[1] <- kaw.o*Aaw/Vw*(c["Ca"]/(Kaw.t) - c["Cw"]) + D.pcb.water*Aws*60*60*24/bl/Vw*(Cpw - c["Cw"]) - kb*c["Cw"]
    # dCadt:
   r[2] <- kaw.o*Aaw/Va*(c["Cw"] - c["Ca"]/Kaw.t)
   
@@ -75,18 +79,20 @@ rtm.PCB4 = function(t, c, parms){
 }
 
 # Initial conditions and run function
-cinit <- c(Cw = 0, Ca = 0)
-t.1 <- c(1:100)
+{cinit <- c(Cw = 0, Ca = 0)
+t.1 <- c(1:10)
 # Placeholder values of key parameters
 out.1 <- ode(y = cinit, times = t.1, func = rtm.PCB4) %>%
 as.data.frame() -> out.1
+}
 
-new.out<- out.1 %>%
+{new.out<- out.1 %>%
   gather(variable, value, -time)
-
 new.out <- within(new.out, variable <- factor(variable,
                                               levels = c('Cw', 'Ca')))
+}
 
+# Plot
 ggplot(new.out, aes(x = time, y = value, color = variable)) +
   facet_wrap(vars(variable), scales = "free_y", ncol = 2) +
   geom_line(size = 2) +
