aace4513cf66fdbb7d949d792101c535d5e2c2c6
martie <andres-martinez@uiowa.edu>
2023-01-17 15:20:54 -0600

    change kb

--- PCB4-Passive-Mass-Model.R
+++ PCB4-Passive-Mass-Model.R
@@ -76,7 +76,7 @@ rtm.PCB4 = function(t, c, parms){
   Cpw <- Ct/Kd*1000 # [ng/L]
   
   # Biotransformation rate
-  kb <- 0.178374771 # 1/d, value changes depending on experiment, i.e., control = 0, treatments LB400 = 0.178374771
+  kb <- 0.178374771 # 0.6161 # 1/d, value changes depending on experiment, i.e., control = 0, treatments LB400 = 0.178374771
   
   # flux constant passed through a list called parms
   ro <- parms$ro # m3/d
