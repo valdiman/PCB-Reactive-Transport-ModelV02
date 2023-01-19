6975c859ab6ab739940a56bb2f421c353215ea71
martie <andres-martinez@uiowa.edu>
2023-01-19 12:24:09 -0600

    small changes.

--- SPME.R
+++ SPME.R
@@ -17,6 +17,7 @@ rtm.SPME = function(t, c, parms){
   # Constant passed through a list called parms
   k <- parms$k # m3/d
   ko <- parms$ko # cm/d
+  #ka <- parms$ka ka = 0.089
   
   # derivatives dx/dt are computed below
   r <- rep(0,length(c))
@@ -31,23 +32,21 @@ rtm.SPME = function(t, c, parms){
 }
 
 # Initial conditions and run function
-{cinit <- c(Cw = 100, mSPME = 0)
-t <- c(1:50)
+{cinit <- c(Cw = 100000, mSPME = 0)
+t <- c(1:40)
 # Placeholder values of key parameters
-parms <- list(k = -0.00001, ko = 10)
+parms <- list(k = 0.000, ko = 100)
 out <- ode(y = cinit, times = t, func = rtm.SPME, parms = parms)
 out <- data.frame(out)}
 
-{new.out<- out %>%
+{new.out <- out %>%
   gather(variable, value, -time)
 new.out <- within(new.out, variable <- factor(variable,
                                                 levels = c('Cw', 'mSPME')))}
 
-
 # Plot
 ggplot(new.out, aes(x = time, y = value, color = variable)) +
-  facet_wrap(vars(variable), scales = "free_y", ncol = 2) +
+  facet_wrap(vars(variable), scales = "free_y", nrow =  2) +
   geom_line(linewidth = 2) +
   theme_classic() +
   labs(x = 'time (day)', y = 'Concentration')
-
